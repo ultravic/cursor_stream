d327ea6c8a33b2f5810e38508b21c66caf5c0a92
Victor Picussa <vp16@inf.ufpr.br>
2019-05-31 11:50:29 -0300

    [option_one]Recode to functions

--- client.py
+++ client.py
@@ -6,54 +6,69 @@ import logging
 import pickle
 import sys
 
+# Default variables
 DEFAULT_MCAST_GRP = '224.1.1.1'
 DEFAULT_MCAST_PORT = 5007
 IS_ALL_GROUPS = True
-
 MESSAGES = {
     'usage': '<client> [[-h <server>], [-p <port>], [-g <group>]]'
 }
 
-if '--help' in sys.argv or '-h' not in sys.argv:
-    print('Usage: ' + MESSAGES['usage'])
-    exit(1)
+# Connection initialization function
+def connection(HOST, PORT, GROUP):
+    client_name = socket.gethostname()
+    client = socket.gethostbyname(client_name)
+    server = socket.gethostbyname(HOST)
 
-client_name = socket.gethostname()
-client = socket.gethostbyname(client_name)
-server_name = sys.argv[sys.argv.index('-h') + 1]
-server = socket.gethostbyname(server_name)
-port = DEFAULT_MCAST_PORT
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    except:
+        print("Erro ao criar socket")
+        exit(0)
 
-# Verify if any option is in the arguments
-if len(sys.argv) > 3:
-    if '-p' in sys.argv:
-        port = int(sys.argv[sys.argv.index('-p') + 1])
-    if '-g' in sys.argv:
-        grp = int(sys.argv[sys.argv.index('-g') + 1])
+    if IS_ALL_GROUPS:
+        # on this port, receives ALL multicast groups
+        sock.bind(('', PORT))
+    else:
+        # on this port, listen ONLY to MCAST_GRP
+        sock.bind((GROUP, PORT))
+    mreq = struct.pack('4sl', socket.inet_aton(GROUP), socket.INADDR_ANY)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-if IS_ALL_GROUPS:
-    # on this port, receives ALL multicast groups
-    sock.bind(('', port))
-else:
-    # on this port, listen ONLY to MCAST_GRP
-    sock.bind((DEFAULT_MCAST_GRP, port))
-mreq = struct.pack('4sl', socket.inet_aton(DEFAULT_MCAST_GRP), socket.INADDR_ANY)
+    return sock
 
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+def init(argv):
+    if '--help' in argv or '-h' not in argv:
+        print('Usage: ' + MESSAGES['usage'])
+        exit(0)
 
-# Receive data until CTRL + C
-try:
-    while True:
-        received = sock.recvfrom(4096)
-        data = pickle.loads(received[0])
+    # Verify if any option is in the arguments
+    host = argv[argv.index('-h') + 1]
+    if '-p' in argv:
+        port = int(argv[argv.index('-p') + 1])
+    else:
+        port = DEFAULT_MCAST_PORT
+    if '-g' in argv:
+        grp = int(argv[argv.index('-g') + 1])
+    else:
+        grp = DEFAULT_MCAST_GRP
 
-        # For debug purposes
-        # positionStr = str(data['id']) + ' > ' + 'X: ' + str(data['X_pos']).rjust(4) + ' Y: ' + str(data['Y_pos']).rjust(4)
-        # print(positionStr, end='')
-        # print('\b' * len(positionStr), end='', flush=True)
-except KeyboardInterrupt:
-    print('\nFinalizando cliente...')
-    exit(1)
+    sock = connection(host, port, grp)
+
+    # Receive data until CTRL + C
+    try:
+        while True:
+            received = sock.recvfrom(4096)
+            data = pickle.loads(received[0])
+
+            # For debug purposes
+            positionStr = str(data['id']) + ' > ' + 'X: ' + str(data['X_pos']).rjust(4) + ' Y: ' + str(data['Y_pos']).rjust(4)
+            print(positionStr, end='')
+            print('\b' * len(positionStr), end='', flush=True)
+    except KeyboardInterrupt:
+        print('\nFinalizando cliente...')
+        exit(1)
+
+init(sys.argv)
--- server.py
+++ server.py
@@ -6,59 +6,74 @@ import logging
 import pickle
 import sys
 
+# Default variables
 DEFAULT_MCAST_GRP = '224.1.1.1'
 DEFAULT_MCAST_PORT = 5007
 DEFAULT_MCAST_TTL = 2
-
 MESSAGES = {
     'usage': '<server> [[-p <port>], [-t <ttl>], [-g <group>]]' 
 }
 
-if '--help' in sys.argv:
-    print('Usage: ' + MESSAGES['usage'])
-    exit(1)
+# Connection initialization function
+def connection(PORT, GROUP, TTL):
+    host_name = socket.gethostname()
+    host = socket.gethostbyname(host_name)
 
-host_name = socket.gethostname()
-host = socket.gethostbyname(host_name)
-port = DEFAULT_MCAST_PORT
-ttl = DEFAULT_MCAST_TTL
-grp = DEFAULT_MCAST_GRP
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, TTL)
+    except:
+        print("Erro ao criar socket!")
+        exit(0)
 
-# Verify if any option is in the arguments
-if len(sys.argv) > 1:
-    if '-p' in sys.argv:
-        port = int(sys.argv[sys.argv.index('-p') + 1])
-    if '-t' in sys.argv:
-        ttl = int(sys.argv[sys.argv.index('-t') + 1])
-    if '-g' in sys.argv:
-        grp = int(sys.argv[sys.argv.index('-g') + 1])
+    return sock
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+def init(argv):
+    if '--help' in argv:
+        print('Usage: ' + MESSAGES['usage'])
+        exit(0)
 
-# Send data until CTRL + C
-try:
-    counter = 0
-    while True:
-        # Cursor positions and window size data
-        x, y = pyautogui.position()
-        w, h = pyautogui.size()
+    # Verify if any option is in the arguments
+    if '-p' in argv:
+        port = int(argv[argv.index('-p') + 1])
+    else:
+        port = DEFAULT_MCAST_PORT
+    if '-t' in argv:
+        ttl = int(argv[argv.index('-t') + 1])
+    else:
+        ttl = DEFAULT_MCAST_TTL
+    if '-g' in argv:
+        grp = int(argv[argv.index('-g') + 1])
+    else:
+        grp = DEFAULT_MCAST_GRP
 
-        data = {
-            'id'    : counter,
-            'X_pos' : x,
-            'Y_pos' : y,
-            'width' : w,
-            'height': h
-        }
+    sock = connection(port, grp, ttl)
+    
+    # Send data until CTRL + C
+    try:
+        counter = 0
+        while True:
+            # Cursor positions and window size data
+            x, y = pyautogui.position()
+            w, h = pyautogui.size()
 
-        # For debug purposes
-        # positionStr = 'X: ' + str(data['X_pos']).rjust(4) + ' Y: ' + str(data['Y_pos']).rjust(4)
-        # print(positionStr, end='')
-        # print('\b' * len(positionStr), end='', flush=True)
+            data = {
+                'id'    : counter,
+                'X_pos' : x,
+                'Y_pos' : y,
+                'width' : w,
+                'height': h
+            }
 
-        sock.sendto(pickle.dumps(data), (grp, port))
-        counter += 1
-except KeyboardInterrupt:
-    print('\nFinalizando servidor...')
-    exit(1)
+            # For debug purposes
+            positionStr = 'X: ' + str(data['X_pos']).rjust(4) + ' Y: ' + str(data['Y_pos']).rjust(4)
+            print(positionStr, end='')
+            print('\b' * len(positionStr), end='', flush=True)
+
+            sock.sendto(pickle.dumps(data), (grp, port))
+            counter += 1
+    except KeyboardInterrupt:
+        print('\nFinalizando servidor...')
+        exit(1)
+
+init(sys.argv)
