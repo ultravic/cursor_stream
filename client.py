d4b26c786b3bb1d8c67c434dd0c3f050cadd8d66
Victor Picussa <vic_picussa2@hotmail.com>
2019-05-30 23:23:42 -0300

    [option_one]Cursor data send and read

--- client.py
+++ client.py
@@ -1,22 +1,59 @@
+#!python3
+
 import socket
 import struct
+import logging
+import pickle
+import sys
 
-MCAST_GRP = '224.1.1.1'
-MCAST_PORT = 5007
+DEFAULT_MCAST_GRP = '224.1.1.1'
+DEFAULT_MCAST_PORT = 5007
 IS_ALL_GROUPS = True
+
+MESSAGES = {
+    'usage': '<client> [[-h <server>], [-p <port>], [-g <group>]]'
+}
+
+if '--help' in sys.argv or '-h' not in sys.argv:
+    print('Usage: ' + MESSAGES['usage'])
+    exit(1)
+
+client_name = socket.gethostname()
+client = socket.gethostbyname(client_name)
+server_name = sys.argv[sys.argv.index('-h') + 1]
+server = socket.gethostbyname(server_name)
+port = DEFAULT_MCAST_PORT
+
+# Verify if any option is in the arguments
+if len(sys.argv) > 3:
+    if '-p' in sys.argv:
+        port = int(sys.argv[sys.argv.index('-p') + 1])
+    if '-g' in sys.argv:
+        grp = int(sys.argv[sys.argv.index('-g') + 1])
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 if IS_ALL_GROUPS:
     # on this port, receives ALL multicast groups
-    sock.bind(('', MCAST_PORT))
+    sock.bind(('', port))
 else:
     # on this port, listen ONLY to MCAST_GRP
-    sock.bind((MCAST_GRP, MCAST_PORT))
-mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+    sock.bind((DEFAULT_MCAST_GRP, port))
+mreq = struct.pack('4sl', socket.inet_aton(DEFAULT_MCAST_GRP), socket.INADDR_ANY)
 
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-while True:
-    print("Received: %s"% (sock.recv(10240)).decode('UTF-8'))
+# Receive data until CTRL + C
+try:
+    while True:
+        received = sock.recvfrom(4096)
+        data = pickle.loads(received[0])
+
+        # For debug purposes
+        # positionStr = str(data['id']) + ' > ' + 'X: ' + str(data['X_pos']).rjust(4) + ' Y: ' + str(data['Y_pos']).rjust(4)
+        # print(positionStr, end='')
+        # print('\b' * len(positionStr), end='', flush=True)
+except KeyboardInterrupt:
+    print('\nFinalizando cliente...')
+    exit(1)
--- server.py
+++ server.py
@@ -1,13 +1,64 @@
+#!python3
+
 import socket
+import pyautogui
+import logging
+import pickle
 import sys
 
-MCAST_GRP = '224.1.1.1'
-MCAST_PORT = 5007
-MULTICAST_TTL = 2
+DEFAULT_MCAST_GRP = '224.1.1.1'
+DEFAULT_MCAST_PORT = 5007
+DEFAULT_MCAST_TTL = 2
+
+MESSAGES = {
+    'usage': '<server> [[-p <port>], [-t <ttl>], [-g <group>]]' 
+}
+
+if '--help' in sys.argv:
+    print('Usage: ' + MESSAGES['usage'])
+    exit(1)
+
+host_name = socket.gethostname()
+host = socket.gethostbyname(host_name)
+port = DEFAULT_MCAST_PORT
+ttl = DEFAULT_MCAST_TTL
+grp = DEFAULT_MCAST_GRP
+
+# Verify if any option is in the arguments
+if len(sys.argv) > 1:
+    if '-p' in sys.argv:
+        port = int(sys.argv[sys.argv.index('-p') + 1])
+    if '-t' in sys.argv:
+        ttl = int(sys.argv[sys.argv.index('-t') + 1])
+    if '-g' in sys.argv:
+        grp = int(sys.argv[sys.argv.index('-g') + 1])
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
-while True:
-    data = input()
-    sock.sendto(data.encode('UTF-8'), (MCAST_GRP, MCAST_PORT))
+# Send data until CTRL + C
+try:
+    counter = 0
+    while True:
+        # Cursor positions and window size data
+        x, y = pyautogui.position()
+        w, h = pyautogui.size()
+
+        data = {
+            'id'    : counter,
+            'X_pos' : x,
+            'Y_pos' : y,
+            'width' : w,
+            'height': h
+        }
+
+        # For debug purposes
+        # positionStr = 'X: ' + str(data['X_pos']).rjust(4) + ' Y: ' + str(data['Y_pos']).rjust(4)
+        # print(positionStr, end='')
+        # print('\b' * len(positionStr), end='', flush=True)
+
+        sock.sendto(pickle.dumps(data), (grp, port))
+        counter += 1
+except KeyboardInterrupt:
+    print('\nFinalizando servidor...')
+    exit(1)
